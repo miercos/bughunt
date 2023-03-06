021ad01fecf5868bc9c6ee55ee5f40ca5dd27b2f
Ilmaan ZIa <ilmancc@gmail.com>
2023-03-07 03:06:30 +0530

    Modified Models

--- bug_tracker/admin.py
+++ bug_tracker/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+admin.site.register(Bugs)
+
+
+admin.site.register(Bugs)
+
+admin.site.register(Bugs)
+
+admin.site.register(Bugs)
--- bug_tracker/models.py
+++ bug_tracker/models.py
@@ -4,15 +4,15 @@ import datetime
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    date = models.DateField( default=datetime.date.today)
 
 
-class Author(models.Model):
+class Employee(models.Model):
+    emp_id = models.AutoField(primary_key=True, default=1000)
     name = models.CharField(max_length=100)
     email = models.EmailField()
+    user_level = models.CharField(max_length=20, choices=[('Author', 'Author'), ('Developer', 'Developer'), ('Tester', 'Tester'), ('Admin', 'Admin')  ])
 
-class Developer(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
 
 
 class Bugs(models.Model):
@@ -23,18 +23,18 @@ class Bugs(models.Model):
     problem = models.TextField()
     suggested_fix = models.TextField()
     reproducible = models.BooleanField(default=False)
-    report_by = models.ForeignKey(Author, on_delete=models.CASCADE)
+    report_by = models.ForeignKey(Employee, on_delete=models.CASCADE)
     date = models.DateField( default=datetime.date.today)
     functional_area = models.CharField(max_length=100)
-    assigned_to = models.ForeignKey(Developer, on_delete=models.CASCADE, related_name='bugs_assigned', null=True, blank=True)
+    assigned_to = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='bugs_assigned', null=True, blank=True)
     comment = models.TextField()
     status = models.CharField(max_length=20, choices=[('Open', 'Open'), ('Closed', 'Closed')])
     priority = models.CharField(max_length=20, choices=[('High', 'High'), ('Medium', 'Medium'), ('Low', 'Low')])
     resolution = models.CharField(max_length=100)
-    resolved_by = models.ForeignKey(Developer, on_delete=models.CASCADE)
+    resolved_by = models.ForeignKey(Employee, on_delete=models.CASCADE)
     tested_by = models.CharField(max_length=50)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    created_by = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='bugs_created')
+    created_by = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='bugs_created')
 
 
 
